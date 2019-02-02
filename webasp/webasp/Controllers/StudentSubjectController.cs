2bd6013b3c652edd340193eed445167465d59256
VC <tommy8595>
2019-02-02 11:37:45 +0700

    add page to asp

---
+++ webasp/webasp/Controllers/StudentSubjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webasp.Controllers
+{
+    public class StudentSubjectController : Controller
+    {
+        // GET: StudentSubject
+        public ActionResult Subject()
+        {
+            return View();
+        }
+    }
+}
