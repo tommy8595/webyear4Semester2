fc743a3cba8b37178259202793b295895b2fb22f
sopheanouk <pheanouk007@gmail.com>
2019-02-03 16:38:06 +0700

    no message

--- webasp/webasp/App_Start/RouteConfig.cs
+++ webasp/webasp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace webasp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "AttendanceAndScore", action = "AttAndScore", id = UrlParameter.Optional }
+                defaults: new { controller = "tbl_user", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
