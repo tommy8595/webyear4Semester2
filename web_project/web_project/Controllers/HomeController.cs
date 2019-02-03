75d2b757d1945001e0ab622d007532ea37803af6
sopheanouk <pheanouk007@gmail.com>
2019-02-03 11:29:25 +0700

    no message

--- web_project/web_project/Controllers/HomeController.cs
+++ web_project/web_project/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace web_project.Controllers
 
             return View();
         }
-
+        
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
